33a951a4ca94b5ed566a7971632fe51010630210
ant <duma4.1.0@gmail.com>
2019-11-21 21:54:20 +0200

    create index setting status jobs

---
+++ app/Http/Controllers/Setting/StatusJobsSettingController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Setting;
+
+use App\Http\Controllers\Controller;
+use App\StatusJob;
+use Illuminate\Http\Request;
+
+class StatusJobsSettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $status = StatusJob::orderBy('id', 'asc')->paginate(10);
+        return view('manage.setting.statusJobs.index',['status' => $status]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/manage/setting/statusJobs/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.manage')
+
+@section('content')
+    <div class="container-fluid list-page-row p-4">
+        <div class="columns">
+            <h1 class="title">Manage Status Jobs</h1>
+            <a href="#" class="btn-secondary btn-lg"> Create New Status</a>
+        </div>
+    </div>
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Date Created</th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($status as $stat)
+            <tr>
+                <th scope="row">{{$stat->id}}</th>
+                <td>{{$stat->name}}</td>
+                <td>{{$stat->created_at->toFormattedDateString()}}</td>
+                <td><a class="btn btn-outline-dark" href="#" role="button">Edit</a></td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{ $status->links() }}
+@endsection
