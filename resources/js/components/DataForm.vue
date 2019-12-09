e3b77be2f7234b531456735ad27a8b8630e8d5cf
AlexKasaDo <sasha.kas92@gmail.com>
2019-12-10 00:14:41 +0200

    kasa

---
+++ resources/js/components/DataForm.vue
@@ -0,0 +1,19 @@
+<script>
+    import DatePicker from 'vue2-datepicker';
+    import 'vue2-datepicker/index.css';
+
+    export default {
+        components: { DatePicker },
+        data() {
+            return {
+                time1: null,
+            };
+        },
+    };
+</script>
+
+<template>
+    <div>
+            <date-picker v-model="time1" type="datetime"  range></date-picker>
+    </div>
+</template>
