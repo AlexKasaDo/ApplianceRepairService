022fbf0a1e27c1ab40d86a89213fd7d166ccbeb6
antonDumanskij <duma4.1.0@gmail.com>
2020-02-03 21:52:32 +0200

    done view fields in edit mode, but i didn't do save chance

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -12,7 +12,7 @@ class EventController extends Controller
     public function show(int $id)
     {
 
-        return response()->json(Event::with('job')->where('job_id',$id)->get(),200);
+        return response()->json(Event::with('staff')->where('job_id',$id)->get(),200);
     }
 
     public function index(Request $request){
@@ -51,7 +51,7 @@ class EventController extends Controller
         $event->save();
         $event->staff()->sync($request->assigned, true);
 
-        return response()->json(Event::with('job')->where('job_id',$event->job_id )->get(),200);
+        return response()->json(Event::with('staff')->where('job_id',$event->job_id )->get(),200);
     }
 
     public function update($id, Request $request){
